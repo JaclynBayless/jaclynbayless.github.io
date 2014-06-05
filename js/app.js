98c4ad707876adfe2e0e46cc3cb4a2be6daadf7a
Jaclyn Bayless <jaclynbayless@eagles.ewu.edu>
2014-06-04 21:09:07 -0700

    GIF

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-$(".message").ready(function() {	
-
-	$(this).anima3d({
-		rotateX:"100deg", 
-		rotateY:"100deg", 
-		rotateZ:"100deg"}, 400).anima2d({scale:0.6}, 400);
-
-	$(this).anima3d({
-		rotateX:"0deg", 
-		rotateY:"0deg", 
-		rotateZ:"0deg"}, 400).anima2d({scale:1}, 400);
-
+$( ".plugin" ).click(function() {
+  $('.gifs').gifplayer();
 });
